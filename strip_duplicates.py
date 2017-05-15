7be9ef9d9495adb91bfc4d9c55f0516a5f41b6fa
ivarvanwooning <ivarvanwooning@Ivars-MacBook-Pro.local>
2017-05-15 17:07:18 +0200

    Second commit

---
+++ strip_duplicates.py
@@ -0,0 +1,14 @@
+startups = open('startups.txt')
+
+seen = set()
+unique = []
+
+for name in startups:
+    if name not in seen:
+        unique.append(name)
+        seen.add(name)
+
+print ''.join(map(str, unique))
+
+
+
